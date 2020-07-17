266038980ca745b6763bf60fe8f2935d0cf65c19
SEUNG BEEN LEE <sbstevenlee@gmail.com>
2020-07-17 18:59:36 +0900

    Add files

--- setup.py
+++ setup.py
@@ -11,5 +11,21 @@ setup(
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
     packages=find_packages(),
+    package_data={
+        "stargazer": [
+            "plot.R",
+            "sv.R",
+            "sv_table.txt",
+            "star_table.txt",
+            "snp_table.txt",
+            "phenotype_table.txt",
+            "gene_table.txt",
+            "beagle.27Apr20.b81.jar",
+        ],
+        "stargazer.1kgp": [
+            "hg19/*.vcf.gz",
+            "hg38/*.vcf.gz",
+        ]
+    },
     entry_points={"console_scripts": ["stargazer=stargazer.__main__:main"]}
 )
